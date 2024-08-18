b43a9197e70b21181d44e46c3ff5c624229ad22d
DerHyper <maxrichter15@web.de>
2024-08-18 19:16:51 +0200

    Add Ghostblock

--- Neon-Tower/Assets/_Scripts/Managers/InputManager.cs
+++ Neon-Tower/Assets/_Scripts/Managers/InputManager.cs
@@ -7,6 +7,21 @@ public class InputManager : MonoBehaviour
 {
     [SerializeField]
     private RectTransform _buildingSpawnArea;
+    public static InputManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
     void Update()
     {
         if (Input.GetMouseButtonUp(0))
@@ -18,7 +33,7 @@ public class InputManager : MonoBehaviour
 
     private void TryBuildBuilding()
     {
-        if(IsMouseInsideSpawnArea(out Vector2 currentPosition) && IsNotBlocked())
+        if (IsMouseInsideSpawnArea(out Vector2 currentPosition) && IsNotBlocked())
         {
             SpawnManager.Instance.TrySpawnBuilding(currentPosition);
         }
@@ -27,10 +42,14 @@ public class InputManager : MonoBehaviour
     private bool IsNotBlocked()
     {
         return !_buildingSpawnArea.gameObject.GetComponent<SpawnArea>().IsBlocked;
-        
+
     }
 
-    private bool IsMouseInsideSpawnArea(out Vector2 currentPosition)
+    public bool IsMouseInsideSpawnArea()
+    {
+        return IsMouseInsideSpawnArea(out Vector2 currentPosition);
+    }
+    public bool IsMouseInsideSpawnArea(out Vector2 currentPosition)
     {
         currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         bool isMouseInsideBox = RectTransformUtility.RectangleContainsScreenPoint(_buildingSpawnArea, currentPosition);
--- Neon-Tower/Assets/_Scripts/Managers/InventoryManager.cs
+++ Neon-Tower/Assets/_Scripts/Managers/InventoryManager.cs
@@ -7,5 +7,6 @@ public class InventoryManager : MonoBehaviour
     public void Select(BuildingInfo Building)
     {
         SpawnManager.Instance.currentBuilding = Building;
+        GhostManager.Instance.UpdateCurrentBuilding(Building);
     }
 }
